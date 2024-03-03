7d8c2db98c7b0db4cc01a26e0570703cdbeaafa5
Robert Risk <robert.risk@annkissam.com>
2024-03-03 16:16:40 -0500

    update button color and tagline

--- js/animationsOnLoad.js
+++ js/animationsOnLoad.js
@@ -8,16 +8,16 @@ setTimeout(function() {
     inspireButton.classList.add('transitioning');
 
     var consultButton = document.getElementById('consultButton');
-    consultButton.classList.remove('btn-outline-warning');
-    consultButton.classList.add('btn-warning');
+    consultButton.classList.remove('btn-outline-success');
+    consultButton.classList.add('btn-success');
     consultButton.addEventListener('transitionend', function() {
         consultButton.classList.remove('transitioning');
     });
     consultButton.classList.add('transitioning');
 
     var talkButton = document.getElementById('talkButton');
-    talkButton.classList.remove('btn-outline-warning');
-    talkButton.classList.add('btn-warning');
+    talkButton.classList.remove('btn-outline-success');
+    talkButton.classList.add('btn-success');
     talkButton.addEventListener('transitionend', function() {
         talkButton.classList.remove('transitioning');
     });
